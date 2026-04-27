d45e2307b1e24b5087e7d984ba911014a8586384
24johnsg-web <24johnsg@mountainview.school.nz>
2026-04-28 09:03:59 +1200

    Create Quiz assesment.py

---
+++ .github/Quiz assesment.py
@@ -0,0 +1,9 @@
+#this is my assesment questionaire 
+
+import time 
+
+print("Hi there thank you for particeptating im my quiz")
+time.sleep(0.5)
+NAME=input("Please enter your name. ")
+time.sleep(0.5)
+print("Thank you "+NAME)
