d41d5969bcee7169655fb04e4bdd999db9487115
24johnsg-web <24johnsg@mountainview.school.nz>
2026-04-29 12:13:32 +1200

    Update Quiz assesment.py

--- .github/Quiz assesment.py
+++ .github/Quiz assesment.py
@@ -5,17 +5,16 @@ import time
 print("Hi there thank you for particeptating im my quiz")
 time.sleep(0.5)            #this is a delay do make the program wait for 0.5 seconds
 NAME=input("Please enter your full name. ")    #this line asks the players name and stores it in a variable called NAME
-FIRSTNAME=NAME.split(" ")
-print(FIRSTNAME)
+FIRSTNAME=NAME.split(" ")[0]
 time.sleep(0.5)
-print("Thank you "+NAME)
+print("Thank you "+FIRSTNAME)
 time.sleep(0.5)
 print("For this quiz we require you age.")
 AGE=int(input("Please enter your age. "))
 if AGE >=14 :
-    print("Sorry "+NAME+", you're too old for this quiz, why don't you check out our 14+ quiz.")
+    print("Sorry "+FIRSTNAME+", you're too old for this quiz, why don't you check out our 14+ quiz.")
 elif AGE <=7 :
-    print("Sorry "+NAME+", you're too young for this quiz, why don't you check out our under 8s quiz.")
+    print("Sorry "+FIRSTNAME+", you're too young for this quiz, why don't you check out our under 8s quiz.")
     time.sleep(0.5)
 else:
     print("Your sweet as to continue. ")
